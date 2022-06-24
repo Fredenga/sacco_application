1cf90a776bc61625a1a4eab09a1e6205df5106be
smiley-geek <kariukigeorge2030@gmail.com>
2022-06-24 17:22:40 +0300

    Update: loggedout state

--- components/Products.tsx
+++ components/Products.tsx
@@ -1,9 +1,85 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import loansService from "../src/graphql/services/loansService";
 
-function Products() {
-  return (
-    <div>Product</div>
-  )
+interface Data {
+  name: string;
+  guarantor: boolean;
+  interestRate: number;
+  maxLoan: number;
+  repayPeriod: number;
 }
 
-export default Products
+const Productss = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  overflow-y: auto;
+  display: flex;
+  flex-direction: column;
+`;
+const Title = styled.div`
+  font-size: 30px;
+  margin: auto;
+  margin-bottom: 5px;
+  margin-top: 5px;
+`;
+
+const List = styled.div`
+  display: flex;
+  width: 100%;
+  flex-wrap: wrap;
+  padding: 30px;
+`;
+
+const ListItem = styled.div`
+  width: 300px;
+  height: 400px;
+  box-shadow: 0px 0px 5px 5px lightgray;
+  margin: 10px;
+  border-radius: 10px;
+  padding: 10px;
+`;
+
+const Text = styled.li`
+  padding: 10px;
+  font-size: larger;
+
+  list-style: circle;
+`;
+
+function Products() {
+  const [loans, setLoans] = useState<Data[]>([]);
+
+  useEffect(() => {
+    const fetchLoans = async () => {
+      const data = await loansService.getAllLoanTypes();
+
+      setLoans(data.getAllLoanTypes);
+    };
+    fetchLoans();
+  }, []);
+  return (
+    <Productss>
+      <Title>Our loan products</Title>
+      <List>
+        {loans.map((product) => (
+          <ListItem>
+            <Title>{product.name} loan</Title>
+            <Text>
+              {product.guarantor
+                ? "Requires guarantors if amount borrowed exceeds savings balance"
+                : "No guarantors required"}
+            </Text>
+            <Text>{product.interestRate}% interest rate</Text>
+            <Text>
+              Maximum loan offer is upto {product.maxLoan}% of savings
+            </Text>
+            <Text>Repayment period of upto {product.repayPeriod} months</Text>
+          </ListItem>
+        ))}
+      </List>
+    </Productss>
+  );
+}
+
+export default Products;
--- layouts/LoggedOut.tsx
+++ layouts/LoggedOut.tsx
@@ -1,25 +1,37 @@
-import { useViewportSize } from "@mantine/hooks";
 import React from "react";
 import styled from "styled-components";
 import Footer from "../components/Footer";
+import Link from "next/link";
 
 const Container = styled.div`
-  height: 100vh;
-  width: 100vw;
+  min-height: 100vh;
+  overflow-x: hidden;
+  width: 100%;
+  position: relative;
+  display: flex;
+  flex-direction: column;
 `;
 
 const Navbar = styled.div`
-  width: 100vw;
-  height: 150px;
-  padding: 20px;
+  box-shadow: 0px 0px 5px 2px lightgray;
+  width: 100%;
+  height: 80px;
+  padding: 30px;
   display: flex;
   justify-content: space-between;
   align-items: center;
+  font-size: 20px;
+  margin-bottom: 20px;
 `;
 
 const Text = styled.div`
-  height: 50px;
+  font-size: 20px;
   margin: 4px;
+  cursor: pointer;
+  opacity: 0.8;
+  &:hover {
+    opacity: 1;
+  }
 `;
 
 const Links = styled.div`
@@ -32,13 +44,23 @@ function LoggedOut({ children }: { children: any }) {
       <Navbar>
         <Text>Esacco</Text>
         <Links>
-          <Text>home</Text>
-          <Text>products</Text>
-          <Text>about</Text>
+          <Link href="/">
+            <Text>home</Text>
+          </Link>
+          <Link href="/product">
+            <Text>products</Text>
+          </Link>
+          <Link href="/about">
+            <Text>about</Text>
+          </Link>
         </Links>
         <Links>
-          <Text>login</Text>
-          <Text>register</Text>
+          <Link href="/login">
+            <Text>login</Text>
+          </Link>
+          <Link href="/register">
+            <Text>register</Text>
+          </Link>
         </Links>
       </Navbar>
       {children}
